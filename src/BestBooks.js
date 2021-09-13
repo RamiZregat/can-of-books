46c61c90f2161b830c28a25f61de8eb3c70bfb42
RamiZregat <rami.zregat97@gmail.com>
2021-09-13 20:49:14 +0300

    done lab12

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,19 +1,64 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Jumbotron from 'react-bootstrap/Jumbotron';
-import './BestBooks.css';
-
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./BestBooks.css";
+import axios from "axios";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Carousel from "react-bootstrap/Carousel";
+import { withAuth0 } from "@auth0/auth0-react";
 class MyFavoriteBooks extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      BookArray: [],
+    };
+  }
+  componentDidMount = () => {
+    const URL = "http://localhost:3010/books";
+    axios
+      .get(URL)
+      .then((result) => {
+        this.setState({
+          BookArray: result.data,
+        });
+        // console.log(this.state.BookArray);
+      })
+      .catch((err) => {
+        console.log("Error");
+      });
+  };
+
   render() {
-    return(
+    return (
       <Jumbotron>
+        <div class={"textDiv"}>
         <h1>My Favorite Books</h1>
-        <p>
-          This is a collection of my favorite books
-        </p>
+        <p>This is a collection of my favorite books</p>
+        </div>
+        <Carousel fade>
+          {this.state.BookArray.map((item) => {
+            return (
+              <Carousel.Item>
+                <div class={'imgDiv'}>
+                <img
+                  src={item.posterURL}
+                  alt="First slide"
+                />
+                </div>
+                <Carousel.Caption>
+                <div class={"textDiv"}>
+                  <h3>Book Name: {item.title}</h3>
+                  <p>
+                  Description: {item.description}
+                  </p>
+                  </div>
+                </Carousel.Caption>
+              </Carousel.Item>
+            );
+          })}
+        </Carousel>
       </Jumbotron>
-    )
+    );
   }
 }
 
-export default MyFavoriteBooks;
+export default withAuth0(MyFavoriteBooks);
